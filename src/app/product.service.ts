a9b7b0e2b8cee55c60bae690301bf63ed885a774
adeyemo bamidele victor <vicar2004@yahoo.com>
2021-11-12 11:04:17 +0100

    commit additional updates

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
+
 import {Http, Response } from '@angular/http';
-import { map } from 'rxjs/operator/map';
-import { AlbumStorePage } from '../../e2e/app.po';
+
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -11,7 +12,8 @@ export class ProductService {
    constructor(private _http: Http) { }
 
    getAlbum(id: number) {
-     return this._http.get(this._albumUrl)
+     return this._http.get(this._albumUrl).map((response) =>
+     Response.json());
    }
 
 }
