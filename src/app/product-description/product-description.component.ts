f3b0fe38089b9e5e6d3ce60008649ac059758714
adeyemo bamidele victor <vicar2004@yahoo.com>
2021-11-12 10:45:25 +0100

    updated commit

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,29 +8,14 @@ import { ProductService } from '../product.service';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
- albumInfo;
 
-  constructor( private _productService: ProductService) {
-    
+  albumInfo;
+
+  constructor( private _productService: ProductService) { }
+
+   ngOnInit() {
+     this._productService.getAlbum(1).subscribe(response =>
+     this.albumInfo = response);
    }
-
-   ngOnInit(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    if (id) {
-      this.getAlbum(1);
-    }
-  }
-
-  getProduct(id: number): void {
-    this._productService.getAlbum(1).subscribe({
-      next: response => this.albumInfo = response,
-      error: err => this.errorMessage = err
-    });
-  }
-
-  onBack(): void {
-    this.router.navigate(['/products']);
-  }
-}
 
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,29 +1,17 @@
 import { Injectable } from '@angular/core';
-import {Http } from '@angular/http';
-import { Response } from '@angular/http';
+import {Http, Response } from '@angular/http';
 import { map } from 'rxjs/operator/map';
 import { AlbumStorePage } from '../../e2e/app.po';
-import { rxjs/add/operator/map } from @angu
 
 @Injectable()
 export class ProductService {
 
-   constructor() { 
-    private_http: Http;
+  private _albumUrl = '../assets/album.json';
 
-  }
+   constructor(private _http: Http) { }
+
+   getAlbum(id: number) {
+     return this._http.get(this._albumUrl)
+   }
 
 }
-
-class private _albumUrl() {
-
-  
-  _albumUrl = ../assets/AlbumStorePage.json;
-  
-}
-
-getAlbum(id: number) ( => {
-  return( this._http.get(_albumUrl))
-  Response.json();
-}); 
-
