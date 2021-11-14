4fbb4f698ad8194255223c9c239631b8ae40c465
adeyemo bamidele victor <vicar2004@yahoo.com>
2021-11-14 16:55:48 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,13 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { RouterModule, Routes, RoutesRecognized } from '@angular/router';
-import { identifierModuleUrl } from '@angular/compiler';
+import { RouterModule, Routes } from '@angular/router';
+
 
 const appRoutes:Routes = [
-  { path: 'products', component: ProductListComponent },
+  { path: 'product', component: ProductListComponent },
   { path: 'product/:id', component: ProductPageComponent },
-  { path: '', redirectTo: 'products', pathMatch: 'full' }
+  { path: '', redirectTo: 'product', pathMatch: 'full' }
 ];
 
 @NgModule({
